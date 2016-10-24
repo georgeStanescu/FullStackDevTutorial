d655c650e72305f33e2423f8d6c4e54ca60109fb
gstanescu <gstanescu@outlook.com>
2016-10-24 17:41:13 +0300

    implemented the add attendance feature

---
+++ GigHub/Controllers/AttendanceController.cs
@@ -0,0 +1,44 @@
+﻿using GigHub.DTOs;
+using GigHub.Models;
+using Microsoft.AspNet.Identity;
+using System.Linq;
+using System.Web.Http;
+
+namespace GigHub.Controllers
+{
+    [Authorize]
+    public class AttendanceController : ApiController
+    {
+        private ApplicationDbContext _context;
+
+        public AttendanceController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        [HttpPost]
+        public IHttpActionResult Attend(AttendanceDto dto)
+        {
+            var userId = User.Identity.GetUserId();
+
+            var existentAttendance = _context.Attendances
+                .Any(a => a.AttendeeId == userId && a.GigId == dto.GigId);
+
+            if (existentAttendance)
+            {
+                return BadRequest("The attendance already exists!");
+            }
+
+            var attendance = new Attendance
+            {
+                GigId = dto.GigId,
+                AttendeeId = userId
+            };
+
+            _context.Attendances.Add(attendance);
+            _context.SaveChanges();
+
+            return Ok();
+        }
+    }
+}
--- GigHub/Global.asax.cs
+++ GigHub/Global.asax.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
@@ -12,6 +13,17 @@ namespace GigHub
     {
         protected void Application_Start()
         {
+            GlobalConfiguration.Configure(config => 
+            {
+                config.MapHttpAttributeRoutes();
+
+                config.Routes.MapHttpRoute(
+                    name: "DefaultApi",
+                    routeTemplate: "api/{controller}/{id}",
+                    defaults: new { id = RouteParameter.Optional }
+                );
+            });
+
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
