5664869006506ea9cd6b12119cba9f5a645f718d
gstanescu <gstanescu@outlook.com>
2016-10-24 18:07:21 +0300

    AddFollowing feature(incomplete)

---
+++ GigHub/Controllers/FollowingController.cs
@@ -0,0 +1,44 @@
+﻿using GigHub.DTOs;
+using GigHub.Models;
+using Microsoft.AspNet.Identity;
+using System.Linq;
+using System.Web.Http;
+
+namespace GigHub.Controllers
+{
+    [Authorize]
+    public class FollowingController : ApiController
+    {
+        private ApplicationDbContext _context;
+
+        public FollowingController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        [HttpPost]
+        public IHttpActionResult Follow(FollowingDto dto)
+        {
+            var userId = User.Identity.GetUserId();
+
+            var existentAttendance = _context.Followings
+                .Any(f => f.FolloweeId == userId && f.FolloweeId == dto.GigId);
+
+            if (existentAttendance)
+            {
+                return BadRequest("Following already exists!");
+            }
+
+            var attendance = new Attendance
+            {
+                GigId = dto.GigId,
+                AttendeeId = userId
+            };
+
+            _context.Attendances.Add(attendance);
+            _context.SaveChanges();
+
+            return Ok();
+        }
+    }
+}
