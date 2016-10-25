0b75a18dfc243c46494d229c08ef2faa3efa0a37
gstanescu <gstanescu@outlook.com>
2016-10-25 15:52:11 +0300

    implemented the update gig operation

--- GigHub/ViewModels/GigFormViewModel.cs
+++ GigHub/ViewModels/GigFormViewModel.cs
@@ -1,12 +1,17 @@
-﻿using GigHub.Models;
+﻿using GigHub.Controllers;
+using GigHub.Models;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.Linq.Expressions;
+using System.Web.Mvc;
 
 namespace GigHub.ViewModels
 {
     public class GigFormViewModel
     {
+        public int Id { get; set; }
+
         [Required]
         public string Venue { get; set; }
 
@@ -26,6 +31,21 @@ namespace GigHub.ViewModels
         public DateTime GetDateTime()
         {
             return DateTime.Parse($"{Date} {Time}");
+        }
+
+        public string Heading { get; set; }
+
+        public string Action {
+            get
+            {
+                //Expression<Func<GigsController, ActionResult>> update = c => c.Update(this);
+                //Expression<Func<GigsController, ActionResult>> create = c => c.Create(this);
+
+                //var action = Id != 0 ? update : create;
+                //var actionName = (action.Body as MethodCallExpression).Method.Name;
+
+                return Id != 0 ? nameof(GigsController.Update) : nameof(GigsController.Create);
+            }
         }
     }
 }
