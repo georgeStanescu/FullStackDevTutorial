2a4a29669b6aae63831943c6d4199b86ab6b8eac
gstanescu <gstanescu@outlook.com>
2016-10-27 14:04:28 +0300

    organised attendance js code into modules

---
+++ GigHub/Scripts/app/controllers/gigsController.js
@@ -0,0 +1,32 @@
+﻿var GigsController = function (attendanceService) {
+    var button;
+
+    var init = function (container) {
+        $(container).on("click", ".js-toggle-attendance", toggleAttendance);
+    };
+
+    var toggleAttendance = function (event) {
+        button = $(event.target);
+
+        var gigId = button.attr("data-gig-id");
+
+        if (button.hasClass("btn-default"))
+            attendanceService.createAttendance(gigId, onDone, onFail);
+        else
+            attendanceService.deleteAttendance(gigId, onDone, onFail);
+    };
+
+    var onFail = function () {
+        alert("Something failed");
+    };
+
+    var onDone = function () {
+        var text = (button.text() === "Going") ? "Going?" : "Going";
+
+        button.toggleClass("btn-info").toggleClass("btn-default").text(text);
+    };
+
+    return {
+        init: init
+    }
+}(AttendanceService);
---
+++ GigHub/Scripts/app/services/attendanceService.js
@@ -0,0 +1,21 @@
+﻿var AttendanceService = function () {
+    var createAttendance = function (gigId, onDone, onFail) {
+        $.post("/api/attendance", { "gigId": gigId })
+           .done(onDone)
+           .fail(onFail);
+    };
+
+    var deleteAttendance = function (gigId, onDone, onFail) {
+        $.ajax({
+            url: "/api/attendance/" + gigId,
+            method: "DELETE"
+        })
+        .done(onDone)
+        .fail(onFail)
+    };
+
+    return {
+        createAttendance: createAttendance,
+        deleteAttendance: deleteAttendance
+    }
+}();
