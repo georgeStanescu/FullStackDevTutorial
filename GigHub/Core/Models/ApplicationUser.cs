26a7acd5fd0583625e0968a671d47859a8fb3f18
gstanescu <gstanescu@outlook.com>
2016-10-28 17:36:28 +0300

    used fluent API to create configuration files to avoid polluting the models with attributes

--- GigHub/Core/Models/ApplicationUser.cs
+++ GigHub/Core/Models/ApplicationUser.cs
@@ -2,18 +2,14 @@
 using System.Threading.Tasks;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
-using System.ComponentModel.DataAnnotations;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
-using System;
 
 namespace GigHub.Core.Models
 {
     // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit http://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
     public class ApplicationUser : IdentityUser
     {
-        [Required]
-        [StringLength(100)]
         public string Name { get; set; }
 
         public ICollection<Following> Followers { get; set; }
